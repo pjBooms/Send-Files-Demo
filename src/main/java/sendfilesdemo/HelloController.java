9d1530eeeb2cb73bcd00ad7df764db4660162764
kit <nlipsky@excelsior-usa.com>
2014-02-15 19:07:33 +0700

    First commit

---
+++ src/main/java/sendfilesdemo/HelloController.java
@@ -0,0 +1,18 @@
+package sendfilesdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class HelloController {
+
+    @ResponseBody
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello() {
+		return "hello";
+	}
+}
